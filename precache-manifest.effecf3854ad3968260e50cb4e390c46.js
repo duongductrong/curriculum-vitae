ba09ef6fbf77faa5790fe4f1b5de4748695edd2f
Duong Duc Trong <duongductrong06@Gmail.com>
2020-06-25 09:21:55 +0700

    Updates

---
+++ precache-manifest.effecf3854ad3968260e50cb4e390c46.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a56958a633ef915758b63d8f85f0f8d",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "506c52fc683314ca94b7",
+    "url": "/curriculum-vitae/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4a4aeefd5cacba8e3a5a",
+    "url": "/curriculum-vitae/static/css/3.ae930102.chunk.css"
+  },
+  {
+    "revision": "43ff308c19aadad5fac5",
+    "url": "/curriculum-vitae/static/css/4.f4e2b73c.chunk.css"
+  },
+  {
+    "revision": "30ca0b13dbaed98e2380",
+    "url": "/curriculum-vitae/static/css/5.f60dee4d.chunk.css"
+  },
+  {
+    "revision": "c3e7767d423f8348d180",
+    "url": "/curriculum-vitae/static/css/6.531b9613.chunk.css"
+  },
+  {
+    "revision": "02b74d142ad1bf3d101b",
+    "url": "/curriculum-vitae/static/css/7.ac4d9986.chunk.css"
+  },
+  {
+    "revision": "72a9b4bd74b64388e52d",
+    "url": "/curriculum-vitae/static/css/main.c55ea266.chunk.css"
+  },
+  {
+    "revision": "506c52fc683314ca94b7",
+    "url": "/curriculum-vitae/static/js/2.f5a9e9a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/curriculum-vitae/static/js/2.f5a9e9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4aeefd5cacba8e3a5a",
+    "url": "/curriculum-vitae/static/js/3.520063b0.chunk.js"
+  },
+  {
+    "revision": "43ff308c19aadad5fac5",
+    "url": "/curriculum-vitae/static/js/4.fed54527.chunk.js"
+  },
+  {
+    "revision": "30ca0b13dbaed98e2380",
+    "url": "/curriculum-vitae/static/js/5.c7e5bfe5.chunk.js"
+  },
+  {
+    "revision": "c3e7767d423f8348d180",
+    "url": "/curriculum-vitae/static/js/6.c879828e.chunk.js"
+  },
+  {
+    "revision": "02b74d142ad1bf3d101b",
+    "url": "/curriculum-vitae/static/js/7.18350091.chunk.js"
+  },
+  {
+    "revision": "72a9b4bd74b64388e52d",
+    "url": "/curriculum-vitae/static/js/main.eca673b8.chunk.js"
+  },
+  {
+    "revision": "29ba02285508e3af7a64",
+    "url": "/curriculum-vitae/static/js/runtime-main.0f344c98.js"
+  },
+  {
+    "revision": "2030b1d00e85414f655f1182571e5cb2",
+    "url": "/curriculum-vitae/static/media/avt.2030b1d0.jpg"
+  },
+  {
+    "revision": "da4bfb5086ae14a2740abdd31caa33cd",
+    "url": "/curriculum-vitae/static/media/blog-project.da4bfb50.png"
+  },
+  {
+    "revision": "01db79be3d75eeca7714bad18cf53ff8",
+    "url": "/curriculum-vitae/static/media/ecommere-project.01db79be.png"
+  }
+]);
